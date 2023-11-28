0d642b57e0396c12247c046732f54d2c38958a2c
Lucas Bihannic <bhl3341a@ensfsi.onmicrosoft.com>
2023-11-28 11:05:36 +0100

    Ajout interface principale avec boutons et styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div style="text-align: left; width: 400px">
-    TODO
+  <div id="app">
+    <h1>Jeu du mot</h1>
+    <button @click="startGame">Démarrer une Partie</button>
+    <button @click="showStats">Statistiques</button>
+    <div class="info-section">
+      <p>Bienvenue dans le Jeu du mot! Trouvez le mot mystère en 5 tentatives.</p>
+    </div>
   </div>
 </template>
 
@@ -11,6 +16,8 @@ export default {
 }
 </script>
 
+
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -20,4 +27,24 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+button {
+  margin: 10px;
+  padding: 10px 15px;
+  border: none;
+  background-color: #3498db;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+}
+
+button:hover {
+  background-color: #2980b9;
+}
+
+.info-section {
+  margin-top: 20px;
+}
 </style>
+
